22d590f34c874e0247c7652074bd24a7c6d8945f
Hiroaki Ito <h.ito198701@gmail.com>
2020-12-12 17:50:08 +0900

    update init.sql

--- init.sql
+++ init.sql
@@ -4,10 +4,6 @@ GRANT ALL ON app3.* TO 'app3' identified by '<password>';
 
 /*
   * 管理サイトユーザ
-  * username:ユーザ名
-  * password:パスワード
-  * role:権限
-  * is_active:有効・無効フラグ
 */
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -28,9 +24,6 @@ INSERT INTO `users` (`username`, `password`, `role`) VALUES
 
 /*
   * フォーム管理
-  * form_name:フォーム名
-  * uri_path:フォームのURLパス
-  * is_active:有効・無効フラグ
 */
 
 drop table form_groups;
@@ -49,19 +42,7 @@ CREATE TABLE `form_groups` (
 
 /*
   * フォーム定義
-  *
-  * form_group_id:フォームNp
-  * label_name:フォームのラベル
-  * schema_name:form_valuesのカラム名に利用
-  * input_type:フォームのinput_typeに利用 ex) 1=text, 2=number, 3=tel, 4=email, 5=password, 7=radio, 8=checkbox 9= date
-  * is_required:必須項目フラグ
-  * choice_value:選択肢が必要な場合に選択項目を入力（radio, checkbox, select）
-  * validate:バリデーション
-    正規表現はこちら
-  https://qiita.com/fubarworld2/items/9da655df4d6d69750c06
-  ex) email /^\S+@\S+\.\S+$/
 */
-
 drop table form_items;
 CREATE TABLE `form_items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -78,6 +59,9 @@ CREATE TABLE `form_items` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+/*
+  * 送信（登録）した入力グループ
+*/
 drop table submits;
 CREATE TABLE `submits` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -89,6 +73,9 @@ CREATE TABLE `submits` (
   KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+/*
+  * 送信（登録）した入力値管理
+*/
 drop table submit_values;
 CREATE TABLE `submit_values` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -105,7 +92,7 @@ CREATE TABLE `submit_values` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*
-  * actions 操作ログ
+  * 操作ログ
 */
 CREATE TABLE `actions` (
   `id` int AUTO_INCREMENT,
